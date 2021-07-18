a90f60a29f8e30d0bd2e8faed2e689854a248a71
Wesley Nogueira <wesleysenan@gmail.com>
2021-07-18 00:37:03 -0300

    Adição do vuetify e componente de footer

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <v-main>
+      <TableResult/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import TableResult from './components/TableResult';
+
+export default {
+  name: 'App',
+  components: {
+    TableResult,
+  }
+};
+</script>
---
+++ src/components/TableResult.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <Footer/>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  import Footer from './Footer.vue';
+
+  export default {
+    name: 'TableResult',
+    components: {
+      Footer,
+    },
+
+    data: () => ({
+    }),
+  }
+</script>
